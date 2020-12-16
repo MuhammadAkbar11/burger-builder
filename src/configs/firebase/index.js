8757783537accb5d2038dd016d50d37b065e6a39
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-12-17 00:58:29 +0700

    add firebase

---
+++ src/configs/firebase/index.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDgpae0rxUWygDD7_mQon_p6zVGgp9_ZzI",
+  authDomain: "burger-builder-app-111220.firebaseapp.com",
+  projectId: "burger-builder-app-111220",
+  storageBucket: "burger-builder-app-111220.appspot.com",
+  messagingSenderId: "232326858660",
+  appId: "1:232326858660:web:292ea5477fbe8ab813a331",
+  measurementId: "G-7PMNYH71ZD",
+};
+
+export default firebaseConfig;
