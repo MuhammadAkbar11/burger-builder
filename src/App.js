69c84e99eef87982b42ab1616e9710bb338965fe
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-02-01 09:44:58 +0700

    adding react router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import {
   ThemeProvider,
 } from "@material-ui/core";
 import Layout from "./components/Layouts/Layout";
-import BurgerBuilder2 from "./containers/BurgerBuilder";
+import BurgerBuilder from "./containers/BurgerBuilder";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Checkout from "./containers/Checkout";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,12 +23,15 @@ const useStyles = makeStyles(theme => ({
 
 const theme = createMuiTheme({
   palette: {
+    type: "dark",
     primary: {
       main: "#f5b316",
       dark: "#f5b216c9",
     },
     dark: "#121212",
-    light: "#dae1e7",
+    light: "#f2f2f2",
+    slate: "#8C8C8C",
+    slatelight: "#c9c8c8",
     success: {
       main: "#3ae57f",
     },
@@ -42,7 +47,11 @@ function App() {
       <div className={classes.root}>
         <CssBaseline />
         <Layout>
-          <BurgerBuilder2 />
+          <Switch>
+            <Route path="/builder" exact component={BurgerBuilder} />
+            <Route path="/checkout" component={Checkout} />
+            <Redirect from="/" to="builder" />
+          </Switch>
         </Layout>
       </div>
     </ThemeProvider>
