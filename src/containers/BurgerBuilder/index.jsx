e2277a2db4274da9e871905029305d4800c794ef
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-02-14 14:37:02 +0700

    change the contents of the burger builder container and replace it with nested routes

--- src/containers/BurgerBuilder/index.jsx
+++ src/containers/BurgerBuilder/index.jsx
@@ -1,77 +1,58 @@
 import React, { Fragment } from "react";
-
 import useStyles from "./styles";
-import { Container, Grid, Box } from "@material-ui/core";
-import BuildControls from "../../components/Burger/BuildControls";
-import Burger from "../../components/Burger";
-import OrderModal from "../../components/UI/OrderModal/OrderModal";
-import OrderSummary from "../../components/Burger/OrderSummary";
-import Spinner from "../../components/UI/Spinner/Spinner";
-import Alert from "../../components/UI/Alert";
-import { useHistory } from "react-router-dom";
+import { Container } from "@material-ui/core";
+import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { BurgerActionTypes } from "../../store/actions/types";
+import BuilderSummary from "../../components/Burger/BuilderSummary";
+import Started from "../../components/Builder/GetStarted";
+import BuilderContainer from "../../components/Builder";
 
-const INGREDIENT_PRICES = {
-  salad: 6000,
-  meat: 12000,
-  cheese: 5000,
-  tomato: 3000,
-};
-
-const BurgerBuilder2 = () => {
+const BurgerBuilder2 = props => {
   const classes = useStyles();
 
-  // const [alert, setAlert] = React.useState({
-  //   show: false,
-  //   type: "success",
-  //   title: "Success",
-  //   subTitle: "Your order was successful",
-  // });
-
-  // let orderSummary = (
-  //   <OrderModal
-  //     open={purchasing}
-  //     title="Your Order"
-  //     handleCancelled={() => setPurchasing(false)}
-  //     handleContinue={purchaseContinueHandler}
-  //   >
-  //     <OrderSummary ingredients={ingredients} totalPrice={totalPrice} />
-  //   </OrderModal>
-  // );
-
-  // if (loading) {
-  //   orderSummary = <Spinner open={true} />;
-  // }
-
-  // let containAlert;
-  // if (alert.show) {
-  //   containAlert = (
-  //     <Alert
-  //       type={alert.type}
-  //       show={true}
-  //       title={alert.title}
-  //       subtitle={alert.subTitle}
-  //       onClose={() => setAlert(prevState => ({ ...prevState, show: false }))}
-  //     />
-  //   );
-  // }
+  const setBurgerName = value => {
+    const result = {
+      value: value,
+    };
+    props.onSetBurgerName(result);
+  };
 
   return (
     <Fragment>
       <Container className={classes.root}>
-        <Grid container className={`${classes.container}`}>
-          <Grid item md={7} lg={8} className={classes.item}>
-            <Box className={`${classes.burgerBox}`}>
-              <Burger />
-            </Box>
-          </Grid>
-          <Grid item md={5} lg={4} className={`${classes.item} right`}>
-            <BuildControls />
-          </Grid>
-        </Grid>
+        <Switch>
+          <Route exact path={props.match.path}>
+            <BuilderContainer burgerName={props.burgerName} />
+          </Route>
+          <Route path={`${props.match.path}/start`}>
+            <Started
+              onSetBurgerName={setBurgerName}
+              isStarted={props.burgerName.trim() === ""}
+            />
+          </Route>
+        </Switch>
       </Container>
-      {containAlert}
+      <BuilderSummary />
     </Fragment>
   );
 };
 
-export default BurgerBuilder2;
+const mapStateToProps = state => {
+  return {
+    burgerName: state.burger.name,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSetBurgerName: value => {
+      return dispatch({
+        type: BurgerActionTypes.SET_BURGER_NAME,
+        payload: value,
+      });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BurgerBuilder2);
--- src/containers/BurgerBuilder/styles.jsx
+++ src/containers/BurgerBuilder/styles.jsx
@@ -22,30 +22,6 @@ const useStyles = makeStyles(theme => ({
       },
     },
   },
-  container: {
-    padding: theme.spacing(3),
-    height: "100%",
-  },
-  item: {
-    height: "100%",
-    width: "100%",
-    paddingLeft: theme.spacing(3),
-    "&.right": {
-      [theme.breakpoints.up("md")]: {
-        borderLeft: "1px solid #333",
-      },
-    },
-  },
-  burgerBox: {
-    height: "100%",
-    display: "flex",
-    alignItems: "start",
-    justifyContent: "center",
-    [theme.breakpoints.only("sm")]: {
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  },
 }));
 
 export default useStyles;
