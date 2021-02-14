b91e0f780872c2355325bafc889ff392c2d24c4c
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-02-14 14:35:25 +0700

    create building starti page

---
+++ src/components/Builder/GetStarted/index.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Box, Typography, TextField, Button } from "@material-ui/core";
+import LoopIcon from "@material-ui/icons/Loop";
+import useStyles from "./styles";
+import { Redirect, useHistory } from "react-router-dom";
+
+const GetStarted = props => {
+  const classes = useStyles();
+
+  const { isStarted } = props;
+  const history = useHistory();
+
+  const [burgerName, setBurgerName] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+
+  const submitHandler = () => {
+    setLoading(true);
+
+    props.onSetBurgerName(burgerName);
+    setBurgerName("");
+    setLoading(false);
+    history.push("/builder");
+  };
+
+  let content = (
+    <React.Fragment>
+      <Typography variant="h6" className={classes.title}>
+        Your burger Name?
+      </Typography>
+      <div className={classes.form}>
+        <TextField
+          size="medium"
+          fullWidth
+          onChange={e => setBurgerName(e.target.value)}
+        />
+        <Button
+          size="medium"
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          onClick={submitHandler}
+          disabled={burgerName.trim() === "" ? true : false}
+        >
+          Let's build
+        </Button>
+      </div>
+    </React.Fragment>
+  );
+
+  if (loading) {
+    content = (
+      <div className={classes.loader}>
+        <LoopIcon style={{ fontSize: 70 }} />
+      </div>
+    );
+  }
+
+  return isStarted ? (
+    <Box className={classes.root}>{content}</Box>
+  ) : (
+    <Redirect to="/builder" />
+  );
+};
+
+export default GetStarted;
---
+++ src/components/Builder/GetStarted/styles.jsx
@@ -0,0 +1,67 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: theme.palette.light,
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+
+    marginTop: theme.spacing(3),
+    "& input": {
+      textAlign: "center",
+      fontSize: "1.4rem",
+      letterSpacing: "3px",
+      textTransform: "uppercase",
+    },
+    "& input:focus": {
+      textAlign: "center",
+      color: theme.palette.primary.main,
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "200px",
+    },
+    [theme.breakpoints.only("sm")]: {
+      width: "300px",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+    },
+  },
+  button: {
+    textTransform: "capitalize",
+    marginTop: theme.spacing(3),
+    fontSize: "1rem",
+  },
+
+  loader: {
+    animation: `$loader 1s linear infinite`,
+    animationDirection: "reverse",
+    color: theme.palette.primary.main,
+    width: "auto",
+    height: "auto",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  "@keyframes loader": {
+    form: {
+      transform: "rotate(0deg)",
+    },
+    to: {
+      transform: "rotate(360deg)",
+    },
+  },
+}));
+
+export default useStyles;
